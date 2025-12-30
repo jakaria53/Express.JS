5662d259055f2b3794c099c16addaab0034640a8
jakaria53 <jakariachowdhury53@gmail.com>
2025-12-30 23:15:51 +0600

    update

--- src/server.js
+++ src/server.js
@@ -1,14 +1,4 @@
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-
-const studentRoutes = require("./routes/student.routes");
-app.use("/api/students", studentRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Student API is running");
-});
+const app = require("./app");
 
 app.listen(5001, () => {
   console.log("Server running on port 5001");
